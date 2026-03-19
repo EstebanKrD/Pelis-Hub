bc92694b5dabbd3f88f0620b9455f9d372b16f82
janner escobar <jannerescobar123@gmail.com>
2026-03-19 15:00:23 -0500

    feat: se crea funcionalidades para arrancar el main

--- script/main.js
+++ script/main.js
@@ -1,1 +1,29 @@
-console.log("se crea main");
+
+// PELISHUB — main.js
+// Solo arranca la aplicación
+
+import { state } from './state.js';
+import { obtenerShows } from './service.js';
+import { obtenerItemsPorPagina } from './storage.js';
+import { renderizarTarjetas, iniciarUI } from './ui.js';
+import { configurarBuscador } from './service.js';
+import { configurarFiltros, renderizarPaginacion, paginarShows } from './ui.js';
+
+window.addEventListener('load', async () => {
+  // Iniciar elementos visuales (intro, hamburguesa)
+  iniciarUI();
+
+  const itemsGuardados = obtenerItemsPorPagina();
+  if (itemsGuardados) {
+    state.itemsPorPagina = parseInt(itemsGuardados);
+  }
+
+  const shows = await obtenerShows(0);
+  state.todosLosShows  = shows;
+  state.showsFiltrados = shows;
+
+  renderizarTarjetas(paginarShows());
+  renderizarPaginacion();
+  configurarBuscador();
+  configurarFiltros();
+});
