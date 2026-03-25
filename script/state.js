510510f18df724c94075587b4d33708f7d984cd1
Juan <hjuandiego45@gmail.com>
2026-03-24 20:23:03 -0500

    feat: se crean funcionalidades para guardar informacion de la app

--- script/state.js
+++ script/state.js
@@ -1,1 +1,20 @@
-console.log("se crea state");
+export const state = {
+  // Datos principales
+  todosLosShows: [],
+  showsFiltrados: [],
+
+  // Paginación
+  paginaActual: 0,
+  itemsPorPagina: 12,
+
+  // Filtros y búsqueda
+  busqueda: "",
+  generoSeleccionado: "",
+
+  // UI / estado
+  cargando: false,
+  error: null,
+
+  // Opcional (para futuro)
+  totalPaginas: 0
+};
