9aa5e09b7093dc3376863df003ab3a413c122e70
Juan <hjuandiego45@gmail.com>
2026-03-24 20:06:38 -0500

    feat: se crean las funcionalidades a las consultas de  la api

--- script/service.js
+++ script/service.js
@@ -1,1 +1,29 @@
-console.log("se crea service");
+import { state } from "./state.js";
+import { renderizarTarjetas, renderizarPaginacion, paginarShows } from "./ui.js";
+
+export function configurarBuscador() {
+  const input = document.getElementById("input-busqueda");
+
+  if (!input) return;
+
+  input.addEventListener("input", async (e) => {
+    const valor = e.target.value.trim();
+
+    if (valor === "") {
+      state.showsFiltrados = state.todosLosShows;
+    } else {
+      const res = await fetch(
+        https://api.tvmaze.com/search/shows?q=${valor}
+      );
+      const data = await res.json();
+
+      // ⚠️ IMPORTANTE: la API devuelve { show: {...} }
+      state.showsFiltrados = data.map((item) => item.show);
+    }
+
+    state.paginaActual = 0;
+
+    renderizarTarjetas(paginarShows());
+    renderizarPaginacion();
+  });
+}
